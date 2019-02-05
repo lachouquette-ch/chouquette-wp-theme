6dd5dc1a939cd3317ed99b88917cbee84c6a7bad
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-02-05 23:14:55 +0100

    no need : A script at js/navigation.js that makes your menu a toggled dropdown on small screens (like your phone), ready for CSS artistry. It's enqueued in functions.php.

--- functions.php
+++ functions.php
@@ -109,8 +109,6 @@ add_action( 'after_setup_theme', 'chouquette_content_width', 0 );
 function chouquette_scripts() {
 	wp_enqueue_style( 'chouquette-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'chouquette-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
-
 	wp_enqueue_script( 'chouquette-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
