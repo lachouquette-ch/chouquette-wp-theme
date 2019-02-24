8e6d42a64c8238ffce9e48e5ba132ca2e041b67d
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-02-24 13:29:24 +0100

    add tops and footer

--- src/app.js
+++ src/app.js
@@ -19,4 +19,35 @@ jQuery(function ($) {
         iconBase: 'fa',
         tickIcon: 'fa-check'
     });
+
+    // activate swiper
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 4,
+        spaceBetween: 40,
+        grabCursor: true,
+        centeredSlides: true,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+            },
+            640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10,
+            }
+        }
+    });
 });
