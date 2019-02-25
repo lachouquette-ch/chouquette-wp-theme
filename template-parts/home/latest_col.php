350f60cb3c5c36f4c96f9056719f60709a8d120a
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-02-25 17:14:18 +0100

    latest posts cannot use bootstrap card : need to use background image for centering / croping

--- template-parts/home/latest_col.php
+++ template-parts/home/latest_col.php
@@ -1,9 +1,8 @@
 <div class="<?php echo 'col-lg-' . $col_size ?> py-1 p-xl-3">
-    <article class="card bg-dark text-white">
-        <?php the_post_thumbnail( 'medium', 'class=card-img' ); ?>
-        <div class="card-img-overlay">
-            <i class="card-category-icon fas fa-theater-masks"></i>
-            <h4 class="card-title"><?php the_title(); ?></h4>
+    <article class="article-card" style="background-image: url('<?php the_post_thumbnail_url( 'medium_large' ) ?>')">
+        <div class="article-card-header p-2">
+            <i class="article-card-category fas fa-theater-masks"></i>
+            <h4 class="article-card-title"><?php the_title(); ?></h4>
         </div>
     </article>
 </div>
