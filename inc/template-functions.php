662ba85e64f6066065c59b9e551b30e7d54068b4
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-03-05 17:17:08 +0100

    #6 Page d'accueil add category to article

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -5,33 +5,55 @@
  * @package Chouquette_thème
  */
 
-if ( ! function_exists( 'chouquette_menu_items' ) ) :
+/**
+ * ChouquetteMenuItem represents a menu item
+ */
+class ChouquetteMenuItem
+{
+    public $id;
+    public $type;
+    public $title;
+    public $description;
+    public $url;
+    public $logo_class;
+}
 
-    class ChouquetteMenuItem {
-        public $id;
-        public $type;
-        public $title;
-        public $description;
-        public $url;
-        public $logo_class;
-
-        function get_post_id() {
-            return $this->type . '_' . $this->id;
-        }
-    }
+if (!function_exists('chouquette_acf_generate_post_id')) :
 
     /**
-     * Retrieve menu items
+     * Generate the post_id as described in https://www.advancedcustomfields.com/resources/get_field/
      */
-    function chouquette_menu_items() {
+    function chouquette_acf_generate_post_id($item)
+    {
+        if ($item instanceof WP_Post) {
+            return $item->ID;
+        } elseif ($item instanceof WP_Term) {
+            return $item->taxonomy . '_' . $item->term_id;
+        } elseif ($item instanceof WP_User) {
+            return 'user_' . $item->ID;
+        } elseif ($item instanceof ChouquetteMenuItem) {
+            return $item->type . '_' . $item->id;
+        }
+        return null;
+    }
+
+endif;
+
+if (!function_exists('chouquette_menu_items')) :
+
+    /**
+     * Retrieve chouquette menu items
+     */
+    function chouquette_menu_items()
+    {
         $result = array();
 
         // get menu items
-        if ( isset ( get_nav_menu_locations()[CHOUQUETTE_PRIMARY_MENU] ) ) {
+        if (isset (get_nav_menu_locations()[CHOUQUETTE_PRIMARY_MENU])) {
             $primary_menu_id = get_nav_menu_locations()[CHOUQUETTE_PRIMARY_MENU];
-            $menu = wp_get_nav_menu_object( $primary_menu_id );
-            $menu_items = wp_get_nav_menu_items( $menu->term_id );
-            foreach ( $menu_items as $menu_item ) :
+            $menu = wp_get_nav_menu_object($primary_menu_id);
+            $menu_items = wp_get_nav_menu_items($menu->term_id);
+            foreach ($menu_items as $menu_item) :
                 // init result object
                 $result_item = new ChouquetteMenuItem();
                 $result_item->id = $menu_item->object_id;
@@ -41,14 +63,50 @@ if ( ! function_exists( 'chouquette_menu_items' ) ) :
                 $result_item->url = $menu_item->url;
 
                 // get logo
-                $result_item->logo_class = get_field( CHOUQUETTE_MENU_LOGO_SELECTOR, $result_item->get_post_id() );
+                $result_item->logo_class = get_field(CHOUQUETTE_MENU_LOGO_SELECTOR, chouquette_acf_generate_post_id($result_item));
 
                 $result[] = $result_item;
             endforeach;
         } else {
-            trigger_error( sprintf("Menu principal du thème '%s' non renseigné", CHOUQUETTE_PRIMARY_MENU), E_USER_WARNING );
+            trigger_error(sprintf("Menu principal du thème '%s' non renseigné", CHOUQUETTE_PRIMARY_MENU), E_USER_WARNING);
         }
 
         return $result;
+    }
+endif;
+
+if (!function_exists('chouquette_top_category')) :
+
+    /**
+     * Retrieve top level category for given post.
+     *
+     * Fails if multiple top categories.
+     */
+    function chouquette_top_category($post_id)
+    {
+        $top_categories = array();
+
+        $categories = get_the_category($post_id);
+        foreach ($categories as $category) {
+            // if already top category
+            if ($category->parent == 1232) { // FIXME should be 0
+                $top_categories[$category->term_id] = $category;
+                continue;
+            }
+            // if not ...
+            $parents = get_ancestors($category->term_id, 'category');
+            foreach (array_reverse($parents) as $term_id) {
+                $parent = get_term($term_id, 'category');
+                if ($parent->parent == 1232) { // FIXME should be 0
+                    $top_categories[$parent->term_id] = $parent;
+                }
+            }
+        }
+
+        // return value
+        if (empty ($top_categories))
+            return null;
+        else
+            return array_pop($top_categories);
     }
 endif;
--- template-parts/article-card.php
+++ template-parts/article-card.php
@@ -1,6 +1,12 @@
 <article class="article-card d-flex align-items-end" style="background-image: url('<?php esc_url( the_post_thumbnail_url( 'medium_large' ) ); ?>')">
     <div class="article-card-header p-2 flex-fill">
-        <i class="article-card-category fas fa-theater-masks"></i>
+        <?php
+            $category = chouquette_top_category( get_the_ID() );
+            if ( isset ( $category )) {
+                $logo_class = get_field(CHOUQUETTE_MENU_LOGO_SELECTOR, chouquette_acf_generate_post_id($category));
+                echo sprintf("<i class='article-card-category %s'></i>", $logo_class);
+            }
+        ?>
         <h3 class="article-card-title"><?php the_title(); ?></h3>
     </div>
 </article>
