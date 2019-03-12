05745726e92bf55952a8d1fdcd69e5d25823e411
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-03-12 19:01:28 +0100

    #8 Page article article header content and fiche

--- single.php
+++ single.php
@@ -12,7 +12,7 @@
 <body>
 
 <header class="cq-header">
-    <nav class="navbar navbar-chouquette navbar-expand-xl sticky-top">
+    <nav class="navbar navbar-chouquette navbar-expand-xl">
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarChouquette" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -56,27 +56,79 @@
 </header>
 
 <article class="container">
-    <header class="single-cover">
-        <img src="" title="post-image">
-        <img src="" title="author">
-        <span>par Cyrielle</span>
-        <span>dans Catégories / ... / ...</span>
-    </header>
-    <h1>Le titre</h1>
-    <main>
-        <aside>
-            <img src="" title="fiche-img">
-            <span>Infos</span>
-        </aside>
-        <p>Mon contenu</p>
-    </main>
-    <footer>
-        <div>Auteur</div>
-        <div>Article similaire</div>
-        <div>Article similaire</div>
-        <div>Commentaire</div>
-    </footer>
+    <div class="row">
+        <header class="col single-header p-0 mt-1 mt-md-4">
+            <img class="single-header-img" src="http://lachouquette.ch/wp-content/uploads/2019/03/Salle-vide-1140x759.jpg" alt="blalbabla">
+            <img class="single-header-author-img rounded-circle" src="http://0.gravatar.com/avatar/ff03948ae1055f3da0cf6b223729cf54?s=140&amp;d=blank&amp;r=g" alt="Justine" width="140"
+                 height="140">
+            <div class="single-header-meta">
+                <span>par Justine</span>
+                <span>le 6 mars 2019</span>
+                <span>dans <a name="desc" href="#">Sorties</a> / <a name="desc" href="#">Culture</a></span>
+            </div>
+        </header>
+    </div>
+    <div class="row">
+        <div class="col-lg-8">
+            <h1>Le titre</h1>
+            <main>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam pellentesque sit amet purus vitae sodales. Nulla vitae libero eget nibh venenatis convallis accumsan non nisl.
+                Suspendisse est magna, commodo et bibendum id, egestas non dui. Nam mauris leo, commodo ut facilisis quis, congue non justo. Vestibulum tempus dapibus massa sed pulvinar. Sed finibus
+                consectetur ante quis blandit. Aenean a vehicula eros, mollis faucibus augue. Fusce venenatis pellentesque nisi.
+
+                Vivamus lacus leo, mollis eget malesuada et, tempus at eros. Nam hendrerit non augue non tristique. Donec dui risus, feugiat nec hendrerit efficitur, blandit a nisi. Vivamus pretium
+                ultrices neque a bibendum. Ut lectus neque, lacinia sit amet imperdiet in, condimentum sit amet purus. Ut ornare erat at dolor rhoncus dignissim. Nunc blandit ligula ut dolor posuere,
+                ac tempor quam venenatis.
+
+                Duis fringilla consequat elit eu tristique. Maecenas magna sapien, efficitur vel fermentum at, aliquam et metus. Pellentesque nec magna ac eros tristique aliquam eget et erat. Ut ac
+                est nibh. Donec condimentum facilisis porttitor. Sed rhoncus viverra est sed fringilla. Pellentesque vel interdum eros. Etiam hendrerit nisi ultricies, facilisis metus ut, aliquet
+                ipsum. Integer quis mauris rutrum magna luctus laoreet vitae eu mauris. Suspendisse potenti.
+
+                Phasellus sollicitudin pulvinar fermentum. Aenean eu nisi tincidunt massa maximus sagittis. Maecenas quis odio pulvinar, vulputate purus laoreet, aliquam metus. Proin quis elit
+                commodo, suscipit tellus id, tempus nisl. Nunc arcu erat, gravida quis elementum sit amet, placerat id erat. Proin sit amet lectus turpis. Aliquam tempus eget sem a pellentesque. Sed
+                velit sem, viverra quis interdum ut, sodales convallis ex. Cras eu dapibus urna, ac ultrices nulla. Aliquam et neque sem. Fusce iaculis massa a ante cursus, a facilisis augue
+                ullamcorper. Fusce aliquet, lorem eget fringilla pretium, eros lectus feugiat sapien, in varius libero magna ut elit. Nam quis ultricies magna. Aenean molestie mi eu sodales luctus.
+
+                Fusce malesuada turpis eget justo tristique, quis suscipit mauris malesuada. Morbi fringilla nulla lacus, eu volutpat justo faucibus et. Cras et mauris non arcu tempor aliquam sit amet
+                quis augue. Fusce interdum nulla quis porta sagittis. Aliquam finibus justo non congue porttitor. Praesent scelerisque rutrum tortor a scelerisque. Curabitur fringilla fermentum nisi
+                sit amet condimentum. Curabitur mattis dignissim arcu, ac dictum metus mollis sit amet. Nullam tincidunt vitae diam vel blandit. Mauris in sapien vel mi pretium malesuada. Pellentesque
+                non volutpat justo, in dapibus lectus. Pellentesque mi lectus, gravida ut leo nec, tempor tincidunt libero.
+            </main>
+        </div>
+        <div class="col-lg-4 mt-2 mt-lg-0 p-1 p-lg-0">
+            <aside>
+                <div class="card">
+                    <img class="card-img-top" src="http://lachouquette.ch/wp-content/uploads/2019/03/JacquesGamblin_NicolasGerardin_4.jpg" alt="Card image cap">
+                    <div class="card-body">
+                        <h4 class="card-title">Théâtre de Beausobre</h4>
+                        <p class="card-text">Le théatre de Morges qui balbalbalbla</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">Avenue de Vertou 2, 1110 Morges</li>
+                        <li class="list-group-item">021 804 15 65</li>
+                        <li class="list-group-item">Site internet</li>
+                        <li class="list-group-item">Email</li>
+                    </ul>
+                    <div class="card-body">
+                        <h5 class="card-subtitle">Infos x</h5>
+                        <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </div>
 </article>
+
+<footer>
+    <div>Auteur</div>
+    <div>Article similaire</div>
+    <div>Article similaire</div>
+    <div>Commentaire</div>
+</footer>
 
 <footer>
     Footer
