522b4b7702d7f84806e44af51252da13d42a0986
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-03-15 15:26:40 +0100

    #8 Page article manage cookies consent

--- comments.php
+++ comments.php
@@ -73,33 +73,40 @@ if (post_password_required()) {
     // as documented in https://codex.wordpress.org/Function_Reference/comment_form
 
     $commenter = wp_get_current_commenter();
-    $req = get_option( 'require_name_email' );
-    $aria_req = ( $req ? " aria-required='true'" : '' );
-    $consent = empty( $commenter['comment_author_email'] ) ? '' : ' checked="checked"';
+    $req = get_option('require_name_email');
+    $aria_req = ($req ? " aria-required='true'" : '');
     $fields = array(
         'author' =>
             '<div class="form-group row">' .
-            '<label class="col-sm-4 col-form-label" for="author">' . __( 'Name' ) . ($req ? '<span class="required">*</span>' : '') . '</label>' .
+            '<label class="col-sm-4 col-form-label" for="author">' . __('Name') . ($req ? '<span class="required">*</span>' : '') . '</label>' .
             '<div class="col-sm-8"><input class="form-control" id="author" name="author" type="text" value="' . esc_attr($commenter['comment_author']) . '" size="30"' . $aria_req . ' /></div>' .
             '</div>',
         'email' =>
             '<div class="form-group row">' .
-            '<label class="col-sm-4 col-form-label" for="email">' . __( 'Email' ) . ($req ? '<span class="required">*</span>' : '') . '</label>' .
+            '<label class="col-sm-4 col-form-label" for="email">' . __('Email') . ($req ? '<span class="required">*</span>' : '') . '</label>' .
             '<div class="col-sm-8"><input class="form-control" id="email" name="email" type="text" value="' . esc_attr($commenter['comment_author_email']) . '" size="30"' . $aria_req . ' /></div>' .
             '</div>',
         'url' =>
             '<div class="form-group row">' .
-            '<label class="col-sm-4 col-form-label" for="url">' . __( 'Website' ) . '</label>' .
+            '<label class="col-sm-4 col-form-label" for="url">' . __('Website') . '</label>' .
             '<div class="col-sm-8"><input class="form-control" id="url" name="url" type="text" value="' . esc_attr($commenter['comment_author_url']) . '" size="30" /></div>' .
             '</div>',
     );
+    // cookies consent
+    if (has_action('set_comment_cookies', 'wp_set_comment_cookies') && get_option('show_comments_cookies_opt_in')) {
+        $consent = empty($commenter['comment_author_email']) ? '' : ' checked="checked"';
+        $fields['cookies'] = '<div class="form-check">' .
+            '<input class="form-check-input" id="wp-comment-cookies-consent" name="wp-comment-cookies-consent" type="checkbox" value="yes"' . $consent . ' />' .
+            '<label for="wp-comment-cookies-consent">' . __('Save my name, email, and website in this browser for the next time I comment.') . '</label>' .
+            '</div>';
+    }
     $args = array(
         'class_submit' => 'btn btn-primary',
         'comment_field' =>
             '<div class="form-group"><label for="comment">' . _x('Comment', 'noun') .
             '</label><textarea class="form-control" id="comment" name="comment" rows="5" aria-required="true"></textarea>' .
             '</div>',
-        'fields' => apply_filters( 'comment_form_default_fields', $fields ),
+        'fields' => apply_filters('comment_form_default_fields', $fields),
         'title_reply_before' => '<h5 id="reply-title" class="comment-reply-title">',
         'title_reply_after' => '</h5>',
     );
