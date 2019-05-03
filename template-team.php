1e02ddec968c25cb780e968b87a85a35cfe5a296
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-05-03 11:46:23 +0200

    #10 Page équipe add page content

--- template-team.php
+++ template-team.php
@@ -6,7 +6,36 @@
 get_header();
 ?>
 
-test
+<div class="container cq-team mb-5">
+    <div class="row cq-team-title">
+        <div class="col text-center">
+            <h1 class="mb-3">L'équipe</h1>
+        </div>
+    </div>
 
 <?php
+$args = array(
+    'role__in' => array('administrator', 'editor', 'author'),
+    'orderby' => 'registered'
+);
+$members = get_users($args);
+foreach ($members as $index => $member) {
+    if ($index % 3 == 0) {
+        echo '<div class="row">';
+    }
+
+    echo '<div class="col-sm-4 p-2">';
+    echo '<div class="text-center p-3 cq-team-member">';
+    echo get_avatar($member->ID, 150, null, $member->display_name, ['class' => 'rounded-circle']);
+    echo '<h5 class="mt-3">' . $member->display_name . '</h5>';
+    echo '<div>' . get_user_meta($member->ID, 'description', true) . '</div>';
+    echo '</div>';
+    echo '</div>';
+
+    if ($index % 3 == 2 || $index +1 == count($members)) {
+        echo '</div>';
+    }
+}
+echo '</div>';
+
 get_footer();
