b4479bc80300442d6f292a9839576baa1999352f
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-06-07 10:30:53 +0200

    #7 Page catégorie new rest api to get taxonomy for given category

--- functions.php
+++ functions.php
@@ -19,9 +19,33 @@ define('CQ_FICHE_SELECTOR', 'link_fiche');
 define('CQ_COOKIE_PREFIX', 'chouquette_');
 
 define('CQ_CATEGORY_BAR_RETOS', 'bar-et-restaurant');
+define('CQ_SUB_CATEGORY_BAR_RETOS_BAR_PUBS', 'bars-pubs');
+define('CQ_SUB_CATEGORY_BAR_RETOS_BOULANGERIES_PATISSERIES', 'boulangeries-patisseries');
+define('CQ_SUB_CATEGORY_BAR_RETOS_RESTAURANTS', 'restaurants');
+define('CQ_SUB_CATEGORY_BAR_RETOS_SUR_LE_POUCE', 'sur-le-pouce');
+define('CQ_SUB_CATEGORY_BAR_RETOS_VIE_NOCTURNE', 'vie-nocturne');
+
 define('CQ_CATEGORY_LOISIRS', 'loisirs');
+define('CQ_SUB_CATEGORY_LOISIRS_ACTIVITES', 'activites');
+define('CQ_SUB_CATEGORY_LOISIRS_BALADES', 'balades');
+define('CQ_SUB_CATEGORY_LOISIRS_ESCAPADES', 'escapades');
+define('CQ_SUB_CATEGORY_LOISIRS_SPA_BIEN_ETRE', 'spa-bien-etre');
+define('CQ_SUB_CATEGORY_LOISIRS_SPORTS', 'sports');
+
 define('CQ_CATEGORY_CULTURE', 'culture-future');
+define('CQ_SUB_CATEGORY_CULTURE_AUTRES', 'culture-autres');
+define('CQ_SUB_CATEGORY_CULTURE_CINEMA', 'cinema-culture-future');
+define('CQ_SUB_CATEGORY_CULTURE_MUSEE', 'musee');
+define('CQ_SUB_CATEGORY_CULTURE_MUSIQUE', 'musique');
+define('CQ_SUB_CATEGORY_CULTURE_THEATRE', 'theatre');
+
 define('CQ_CATEGORY_SHOPPING', 'shopping');
+define('CQ_SUB_CATEGORY_SHOPPING_AUTRE', 'shopping-autre');
+define('CQ_SUB_CATEGORY_SHOPPING_DECO', 'shopping-deco');
+define('CQ_SUB_CATEGORY_SHOPPING_FOOD', 'food');
+define('CQ_SUB_CATEGORY_SHOPPING_MODE', 'shopping-mode');
+define('CQ_SUB_CATEGORY_SHOPPING_WEB', 'shopping-web');
+
 define('CQ_CATEGORY_CHOUCHOUS', 'les-chouchous');
 
 define('CQ_TAXONOMY_BAR_REST_WHEN', 'cq_bar_rest_when');
--- inc/template-api.php
+++ inc/template-api.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * REST API to get all locations for a given post
+ *
+ * @param $data GET params with post 'id'
+ * @return array of object(id, lat, lng)
+ */
 function cq_get_localisations_for_post($data)
 {
     $fiches = get_field(CQ_FICHE_SELECTOR, $data['id']);
@@ -31,5 +37,113 @@ add_action('rest_api_init', function () {
     register_rest_route('cq/v1', '/post/(?P<id>\d+)/localisation', array(
         'methods' => 'GET',
         'callback' => 'cq_get_localisations_for_post',
+    ));
+});
+
+/**
+ * REST API to get all taxonomy for a given category
+ *
+ * @param $data GET params with category 'slug'
+ * @return array of object(label, instructions, name, taxonomy, terms => object(term_id, name, slug, description))
+ */
+function cq_get_taxonomies_for_category($data)
+{
+    function build_result(string $taxonomy)
+    {
+        // field
+        $raw_field = get_field_object($taxonomy, 17895); // 17895 is fiche Buet
+        $field = array_filter($raw_field, function ($key) {
+            return $key == 'label' || $key == 'instructions' || $key == 'name' || $key == 'taxonomy';
+        }, ARRAY_FILTER_USE_KEY);
+
+        // taxonomy
+        $raw_terms = get_terms($field['taxonomy']);
+        $terms = array_map(function (WP_Term $term) {
+            $result = [];
+            $result['term_id'] = $term->term_id;
+            $result['name'] = $term->name;
+            $result['slug'] = $term->slug;
+            $result['description'] = $term->description;
+            return $result;
+
+        }, $raw_terms);
+
+        // put things together
+        $field['terms'] = $terms;
+        return $field;
+    }
+
+    $results = array();
+    switch ($data['slug']) {
+        case CQ_CATEGORY_BAR_RETOS:
+        case CQ_SUB_CATEGORY_BAR_RETOS_VIE_NOCTURNE:
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_CRITERIA);
+            break;
+        case CQ_SUB_CATEGORY_BAR_RETOS_BAR_PUBS:
+            $results[] = build_result(CQ_TAXONOMY_BAR_TYPE);
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_CRITERIA);
+            break;
+        case CQ_SUB_CATEGORY_BAR_RETOS_BOULANGERIES_PATISSERIES:
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_WHEN);
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_WHO);
+            $results[] = build_result(CQ_TAXONOMY_REST_RESTRICTION);
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_CRITERIA);
+            break;
+        case CQ_SUB_CATEGORY_BAR_RETOS_RESTAURANTS:
+        case CQ_SUB_CATEGORY_BAR_RETOS_SUR_LE_POUCE:
+            $results[] = build_result(CQ_TAXONOMY_REST_TYPE);
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_WHEN);
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_WHO);
+            $results[] = build_result(CQ_TAXONOMY_REST_RESTRICTION);
+            $results[] = build_result(CQ_TAXONOMY_BAR_REST_CRITERIA);
+            break;
+
+        case CQ_CATEGORY_LOISIRS:
+        case CQ_SUB_CATEGORY_LOISIRS_ACTIVITES:
+        case CQ_SUB_CATEGORY_LOISIRS_BALADES:
+        case CQ_SUB_CATEGORY_LOISIRS_ESCAPADES:
+        case CQ_SUB_CATEGORY_LOISIRS_SPA_BIEN_ETRE:
+        case CQ_SUB_CATEGORY_LOISIRS_SPORTS:
+            $results[] = build_result(CQ_TAXONOMY_HOBBY);
+            break;
+
+        case CQ_CATEGORY_CULTURE:
+        case CQ_SUB_CATEGORY_CULTURE_AUTRES:
+        case CQ_SUB_CATEGORY_CULTURE_CINEMA:
+        case CQ_SUB_CATEGORY_CULTURE_MUSEE:
+        case CQ_SUB_CATEGORY_CULTURE_MUSIQUE:
+        case CQ_SUB_CATEGORY_CULTURE_THEATRE:
+            $results[] = build_result(CQ_TAXONOMY_CULTURE);
+            break;
+
+        case CQ_CATEGORY_SHOPPING:
+        case CQ_SUB_CATEGORY_SHOPPING_MODE:
+            $results[] = build_result(CQ_TAXONOMY_SHOPPING_MODE);
+            break;
+        case CQ_SUB_CATEGORY_SHOPPING_DECO:
+            $results[] = build_result(CQ_TAXONOMY_SHOPPING_DECO);
+            break;
+        case CQ_SUB_CATEGORY_SHOPPING_FOOD:
+            $results[] = build_result(CQ_TAXONOMY_SHOPPING_FOOD);
+            break;
+        case CQ_SUB_CATEGORY_SHOPPING_AUTRE:
+        case CQ_SUB_CATEGORY_SHOPPING_WEB:
+            $results[] = build_result(CQ_TAXONOMY_SHOPPING_OTHERS);
+            break;
+
+        case CQ_CATEGORY_CHOUCHOUS:
+            $results[] = build_result(CQ_TAXONOMY_CHOUCHOU);
+            break;
+    }
+    // add common criteria
+    $results[] = build_result(CQ_TAXONOMY_CRITERIA);
+
+    return $results;
+}
+
+add_action('rest_api_init', function () {
+    register_rest_route('cq/v1', '/category/(?P<slug>[\w-]+)/taxonomy', array(
+        'methods' => 'GET',
+        'callback' => 'cq_get_taxonomies_for_category',
     ));
 });
