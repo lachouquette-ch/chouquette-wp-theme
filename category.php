9ec119b3bf3a731927ac111220a0d5f21472e0b3
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-06-18 17:44:39 +0200

    #7 Page catégorie map popup design

--- category.php
+++ category.php
@@ -160,6 +160,102 @@ $locations = get_terms(array(
         </div>
     </div>
 
+    <script>
+        var markers = new Map();
+
+        function initMap() {
+            let map = new google.maps.Map(document.getElementById('fichesMap'), {
+                zoom: 15,
+                disableDefaultUI: true,
+                fullscreenControl: true,
+                gestureHandling: 'greedy',
+                restriction: {
+                    latLngBounds: SWITZERLAND_BOUNDS,
+                    strictBounds: false,
+                },
+                styles: MAP_STYLES,
+                center: LAUSANNE_LOCALISATION
+            });
+
+            var contentString = `
+            <div class="category-info-window container-fluid">
+                <div class="row">
+                    <div class="col-lg-3 text-center p-0 mb-2 mb-lg-0">
+                        <img width="75px" class="rounded" src="http://chouquette.test/wp-content/uploads/2018/11/Buet-web-07a-150x150.jpg" alt="Generic placeholder image">
+                    </div>
+                    <div class="col-lg px-0 px-lg-2">
+                        <h5 class="mt-0">Maison Buet – Café Boutique</h5>
+                        <div>
+                            <label>Internet :</label>
+                            <span>
+                                <a class="link-secondary mr-2" href="https://www.facebook.com/maisonbuet/" title="Facebook" target="_blank" class="mr-2"><i class="fab fa-facebook-f"></i></a>
+                                <a class="link-secondary mr-2" href="https://www.instagram.com/maisonbuet/" title="Instagram" target="_blank" class="mr-2"><i class="fab fa-instagram"></i></a>
+                                <a class="link-secondary link-no-decoration mr-2" href="http://www.maisonbuet.ch" title="Site internet" target="_blank"><i class="fas fa-desktop"></i> Site</a>
+                            </span>
+                        </div>
+                        <div>
+                            <label>Contact :</label>
+                            <span>
+                                <a class="link-secondary link-no-decoration mr-1" href="mailto:info@maisonbuet.ch?body=%0A---%0AEnvoy%C3%A9%20depuis%20http://chouquette.test" title="Email"><i class="fas fa-at"></i> Mail</a>
+                                <a class="link-secondary link-no-decoration mr-1" href="tel:021 312 62 90" title="Téléphone"><i class="fas fa-phone"></i> +41 21 312 62 90</a>
+                            </span>
+                        </div>
+                        <div>
+                            <label>Prix :</label>
+                            <span class="cq-fiche-price cq-fiche-price-selected">$$</span><span class="cq-fiche-price">$$$</span>
+                        </div>
+                        <div>
+                            <label>Horaires :</label>
+                            <a class="link-secondary link-no-decoration" data-toggle="collapse" href="#horaires" role="button">Ouvert (12:00-15:00) <i class="fas fa-caret-down"></i></a>
+                            <ul class="collapse" id="horaires">
+                                <li><label>Lundi</label> 12:00-15:00</li>
+                                <li><label>Mardi</label> 12:00-15:00</li>
+                                <li><label>Mercredi</label> 12:00-15:00</li>
+                                <li><label>Jeudi</label> 12:00-15:00</li>
+                                <li><label>Vendredi</label> 12:00-15:00</li>
+                                <li><label>Samedi</label> 12:00-15:00</li>
+                                <li><label>Dimanche</label> 12:00-15:00</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-2">Réserve ton ménage en quelques clics seulement !</div>
+                <div class="row mt-3 font-italic">Accès Wi-Fi, Plats à emporter, Service de livraison, Terrasse...</div>
+            </div>`;
+
+            var infowindow = new google.maps.InfoWindow({
+                content: contentString
+            });
+
+            var marker = new google.maps.Marker({position: LAUSANNE_LOCALISATION, map: map});
+            marker.addListener('click', function () {
+                var self = this;
+                self.setAnimation(google.maps.Animation.BOUNCE);
+                infowindow.open(map, marker);
+                window.setTimeout(function () {
+                    self.setAnimation(null);
+                }, 2000);
+            });
+            //var bounds = new google.maps.LatLngBounds();
+            //$.getJSON('http://chouquette.test/wp-json/cq/v1/post/<?php //echo get_the_ID() ?>///localisation', function (fiches) {
+            //    fiches.forEach(function (fiche) {
+            //        var marker = new google.maps.Marker({position: fiche, map: map});
+            //        marker.addListener('click', function () {
+            //            bounce(markers.get(fiche.id));
+            //            document.getElementById('ficheLink' + fiche.id).click();
+            //        });
+            //        markers.set(fiche.id, marker);
+            //        bounds.extend(marker.getPosition());
+            //    });
+            //    if (markers.size > 1) {
+            //        map.fitBounds(bounds);
+            //    } else {
+            //        map.setCenter(markers.values().next().value.getPosition());
+            //    }
+            //});
+        };
+    </script>
+
     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.0/dist/vue.js"></script>
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
     <script>
@@ -197,43 +293,6 @@ $locations = get_terms(array(
                 this.refreshCriterias(this.category);
             }
         })
-    </script>
-
-    <script>
-        var markers = new Map();
-
-        function initMap() {
-            let map = new google.maps.Map(document.getElementById('fichesMap'), {
-                zoom: 15,
-                disableDefaultUI: true,
-                fullscreenControl: true,
-                gestureHandling: 'greedy',
-                restriction: {
-                    latLngBounds: SWITZERLAND_BOUNDS,
-                    strictBounds: false,
-                },
-                styles: MAP_STYLES,
-                center: LAUSANNE_LOCALISATION
-            });
-
-            //var bounds = new google.maps.LatLngBounds();
-            //$.getJSON('http://chouquette.test/wp-json/cq/v1/post/<?php //echo get_the_ID() ?>///localisation', function (fiches) {
-            //    fiches.forEach(function (fiche) {
-            //        var marker = new google.maps.Marker({position: fiche, map: map});
-            //        marker.addListener('click', function () {
-            //            bounce(markers.get(fiche.id));
-            //            document.getElementById('ficheLink' + fiche.id).click();
-            //        });
-            //        markers.set(fiche.id, marker);
-            //        bounds.extend(marker.getPosition());
-            //    });
-            //    if (markers.size > 1) {
-            //        map.fitBounds(bounds);
-            //    } else {
-            //        map.setCenter(markers.values().next().value.getPosition());
-            //    }
-            //});
-        };
     </script>
 <?php
 get_footer();
