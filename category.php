dd6fb5d27a0e8080f11cbf8d321d130224d526e4
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-06-25 14:32:38 +0200

    #7 Page catégorie fix fiche article link

--- category.php
+++ category.php
@@ -137,10 +137,10 @@ $locations = get_terms(array(
                                     <?php
                                     if (!empty($posts)) {
                                         $lastest_post = $posts[0];
-                                        echo sprintf('<button href="%s" title="%s" class="btn btn-sm btn-outline-secondary">Article</button>', get_the_permalink($lastest_post), esc_html($lastest_post->post_title));
+                                        echo sprintf('<a href="%s" title="%s" class="btn btn-sm btn-outline-secondary">Article</a>', get_the_permalink($lastest_post), esc_html($lastest_post->post_title));
                                     }
                                     ?>
-                                    <button href="" class="btn btn-sm btn-outline-secondary" v-on:click="locateFiche(<?php echo get_the_ID(); ?>)">Voir</button>
+                                    <button class="btn btn-sm btn-outline-secondary" v-on:click="locateFiche(<?php echo get_the_ID(); ?>)">Voir</button>
                                 </div>
                             </div>
                         </article>
