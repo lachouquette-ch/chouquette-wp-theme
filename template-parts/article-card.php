1da11142b84865a39b5e2b95be83e9add8b5ed82
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-08-06 08:50:30 +0200

    refactor

--- template-parts/article-card.php
+++ template-parts/article-card.php
@@ -1,12 +1,11 @@
 <a class="article-card" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
     <?php
     $fiches = chouquette_fiche_get_all();
-    $fiches_chouquettise = array_map(function ($fiche) {
+    $fiches_chouquettises = array_filter($fiches, function ($fiche) {
         return chouquette_fiche_is_chouquettise($fiche->ID);
-    }, $fiches);
-    $is_chouquettise = in_array(true, $fiches_chouquettise);
+    });
     ?>
-    <article class="d-flex align-items-end <?php echo $is_chouquettise ? 'article-card-chouquettise' : ''; ?>" style="background-image: url('<?php esc_url(the_post_thumbnail_url('medium_large')); ?>')">
+    <article class="d-flex align-items-end <?php echo !empty($fiches_chouquettises) ? 'article-card-chouquettise' : ''; ?>" style="background-image: url('<?php esc_url(the_post_thumbnail_url('medium_large')); ?>')">
         <div class="article-card-header p-2 flex-fill">
             <?php
             $categories = chouquette_categories_get_tops(get_the_ID());
