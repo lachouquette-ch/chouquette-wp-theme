3726e0d43284def2992dee2d7f82e1c76a086e91
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-08-08 16:40:27 +0200

    #36 Change info window extra info and links (for chouquettise)

--- inc/api/info-window.php
+++ inc/api/info-window.php
@@ -4,67 +4,74 @@ $fiche_fields = get_fields(get_the_ID());
 $fiche_taxonomies = chouquette_fiche_get_taxonomies($fiche);
 $is_chouquettise = chouquette_fiche_is_chouquettise($fiche->ID);
 ?>
-<div class="category-info-window container-fluid">
+<div class="info-window container-fluid">
     <div class="row">
         <div class="col-lg-3 text-center p-0 mb-2 mb-lg-0">
             <img width="100%" class="rounded d-none d-lg-block" src="<?php esc_url(the_post_thumbnail_url('thumbnail')); ?>" alt="<?php get_the_title($fiche->ID); ?>">
         </div>
         <div class="col-lg px-0 px-lg-2">
-            <h5 class="mt-0"><?php echo get_the_title(); ?></h5>
-            <?php if ($is_chouquettise): ?>
-                <div>
-                    <label>Internet :</label>
-                    <span>
-                    <?php
+            <div class="info-window-header mb-2">
+                <h5 class="d-inline mr-2"><?php echo get_the_title(); ?></h5>
+                <?php if ($is_chouquettise):
                     if (!empty($fiche_fields[CQ_FICHE_FACEBOOK])) echo '<a href="' . esc_url($fiche_fields[CQ_FICHE_FACEBOOK]) . '" title="Facebook" target="_blank" class="link-secondary mr-2"><i class="fab fa-facebook-f"></i></a>';
                     if (!empty($fiche_fields[CQ_FICHE_INSTAGRAM])) echo '<a href="' . esc_url($fiche_fields[CQ_FICHE_INSTAGRAM]) . '" title="Instagram" target="_blank" class="link-secondary mr-2"><i class="fab fa-instagram"></i></a>';
                     if (!empty($fiche_fields[CQ_FICHE_TWITTER])) echo '<a href="' . esc_url($fiche_fields[CQ_FICHE_TWITTER]) . '" title="Twitter" target="_blank" class="link-secondary mr-2"><i class="fab fa-twitter"></i></a>';
                     if (!empty($fiche_fields[CQ_FICHE_PINTEREST])) echo '<a href="' . esc_url($fiche_fields[CQ_FICHE_PINTEREST]) . '" title="Twitter" target="_blank" class="link-secondary mr-2"><i class="fab fa-pinterest-p"></i></a>';
-                    ?>
-                </span>
-                </div>
-                <div>
-                    <label>Contact :</label>
-                    <span>
-                    <?php
-                    if (!empty($fiche_fields[CQ_FICHE_MAIL])) echo sprintf('<a href="mailto:%s" title="Email" class="link-secondary link-no-decoration mr-2"><i class="fas fa-at"></i> Email</a>', $fiche_fields[CQ_FICHE_MAIL] . '?body=%0A---%0AEnvoy%C3%A9%20depuis%20' . get_home_url());
-                    if (!empty($fiche_fields[CQ_FICHE_PHONE])) echo "<a href='tel:{$fiche_fields[CQ_FICHE_PHONE]}' title='Téléphone' class='link-secondary link-no-decoration mr-2'><i class='fas fa-phone-square'></i> {$fiche_fields[CQ_FICHE_PHONE]}</a>";
-                    ?>
-                </span>
-                </div>
-                <div>
+                endif; ?>
+            </div>
+            <?php if ($is_chouquettise): ?>
+                <div class="info-window-extra">
+                    <?php if (!empty($fiche_fields[CQ_FICHE_PHONE])): ?>
+                        <div>
+                            <a href="tel:<?php echo $fiche_fields[CQ_FICHE_PHONE] ?>"
+                               title="Téléphone" class="link-secondary link-no-decoration mr-2">
+                                <i class="fas fa-phone-square mr-3"></i> <?php echo $fiche_fields[CQ_FICHE_PHONE] ?>
+                            </a>
+                        </div>
+                    <?php endif; ?>
+                    <?php if (!empty($fiche_fields[CQ_FICHE_MAIL])): ?>
+                        <div>
+                            <a href="mailto:<?php echo $fiche_fields[CQ_FICHE_MAIL] . '?body=%0A---%0AEnvoy%C3%A9%20depuis%20' . get_home_url() ?>"
+                               title="Email" class="link-secondary link-no-decoration mr-2">
+                                <i class="fas fa-at mr-3"></i> <?php echo $fiche_fields[CQ_FICHE_MAIL] ?>
+                            </a>
+                        </div>
+                    <?php endif; ?>
                     <?php if (!empty($fiche_fields[CQ_FICHE_COST])): ?>
-                        <label>Prix :</label>
-                        <span class="cq-fiche-price cq-fiche-price-selected"><?php echo str_repeat('$', $fiche_fields[CQ_FICHE_COST]); ?></span><span
-                                class="cq-fiche-price"><?php echo str_repeat('$', 5 - $fiche_fields[CQ_FICHE_COST]); ?></span>
+                        <div>
+                            <label>Prix :</label>
+                            <span class="cq-fiche-price cq-fiche-price-selected"><?php echo str_repeat('$', $fiche_fields[CQ_FICHE_COST]); ?></span><span
+                                    class="cq-fiche-price"><?php echo str_repeat('$', 5 - $fiche_fields[CQ_FICHE_COST]); ?></span>
+                        </div>
+                    <?php endif; ?>
+                    <?php
+                    if (chouquette_fiche_has_openings($fiche_fields)):
+                        $raw_planning = array(
+                            $fiche_fields[CQ_FICHE_OPENING_MONDAY],
+                            $fiche_fields[CQ_FICHE_OPENING_TUESDAY],
+                            $fiche_fields[CQ_FICHE_OPENING_WEDNESDAY],
+                            $fiche_fields[CQ_FICHE_OPENING_THURSDAY],
+                            $fiche_fields[CQ_FICHE_OPENING_FRIDAY],
+                            $fiche_fields[CQ_FICHE_OPENING_SATURDAY],
+                            $fiche_fields[CQ_FICHE_OPENING_SUNDAY],
+                        );
+                        ?>
+                        <div>
+                            <label>Horaires :</label>
+                            <a class="link-secondary link-no-decoration" data-toggle="collapse" href="#horaires" role="button"><?php echo $raw_planning[date('N') - 1]; ?> <i
+                                        class="fas fa-caret-down"></i></a>
+                            <ul class="collapse mb-0" id="horaires">
+                                <li><label>Lundi</label> <?php echo $raw_planning[0]; ?></li>
+                                <li><label>Mardi</label> <?php echo $raw_planning[1]; ?></li>
+                                <li><label>Mercredi</label> <?php echo $raw_planning[2]; ?></li>
+                                <li><label>Jeudi</label> <?php echo $raw_planning[3]; ?></li>
+                                <li><label>Vendredi</label> <?php echo $raw_planning[4]; ?></li>
+                                <li><label>Samedi</label> <?php echo $raw_planning[5]; ?></li>
+                                <li><label>Dimanche</label> <?php echo $raw_planning[6]; ?></li>
+                            </ul>
+                        </div>
                     <?php endif; ?>
                 </div>
-                <?php
-                if (chouquette_fiche_has_openings($fiche_fields)):
-                    $raw_planning = array(
-                        $fiche_fields[CQ_FICHE_OPENING_MONDAY],
-                        $fiche_fields[CQ_FICHE_OPENING_TUESDAY],
-                        $fiche_fields[CQ_FICHE_OPENING_WEDNESDAY],
-                        $fiche_fields[CQ_FICHE_OPENING_THURSDAY],
-                        $fiche_fields[CQ_FICHE_OPENING_FRIDAY],
-                        $fiche_fields[CQ_FICHE_OPENING_SATURDAY],
-                        $fiche_fields[CQ_FICHE_OPENING_SUNDAY],
-                    );
-                    ?>
-                    <div>
-                        <label>Horaires :</label>
-                        <a class="link-secondary link-no-decoration" data-toggle="collapse" href="#horaires" role="button"><?php echo $raw_planning[date('N') - 1]; ?> <i class="fas fa-caret-down"></i></a>
-                        <ul class="collapse mb-0" id="horaires">
-                            <li><label>Lundi</label> <?php echo $raw_planning[0]; ?></li>
-                            <li><label>Mardi</label> <?php echo $raw_planning[1]; ?></li>
-                            <li><label>Mercredi</label> <?php echo $raw_planning[2]; ?></li>
-                            <li><label>Jeudi</label> <?php echo $raw_planning[3]; ?></li>
-                            <li><label>Vendredi</label> <?php echo $raw_planning[4]; ?></li>
-                            <li><label>Samedi</label> <?php echo $raw_planning[5]; ?></li>
-                            <li><label>Dimanche</label> <?php echo $raw_planning[6]; ?></li>
-                        </ul>
-                    </div>
-                <?php endif; ?>
             <?php elseif (!empty(get_the_content())) : ?>
                 <?php echo get_the_content(); ?>
             <?php endif; ?>
