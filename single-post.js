0555dcf5c391203a38e02c535eb3abea1440903f
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-08-30 00:11:12 +0200

    minor fix

--- single-post.js
+++ single-post.js
@@ -8,6 +8,9 @@ grecaptcha.ready(function () {
     });
 });
 
+// nothing to do
+function bootstrapMap() {};
+
 var app = new Vue({
     el: '#app',
     mixins: [VUE_UTILITY_MIXIN, VUE_FICHE_MIXIN],
