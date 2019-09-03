74a7301cf805c0663a8d6c2e9615af1754a137f3
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-09-03 13:50:02 +0200

    minor fix

--- template-contact.php
+++ template-contact.php
@@ -9,7 +9,7 @@ while (have_posts()) :
     the_post();
     ?>
 
-    <div class="container cq-contact my-5">
+    <div class="container cq-contact py-5">
         <h1 class="mb-3 cq-font text-center"><?php the_title() ?></h1>
         <div class="mb-5 text-center"><?php the_content(); ?></div>
         <div class="container">
