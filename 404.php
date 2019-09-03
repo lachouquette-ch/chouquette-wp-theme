99a258e99846db54962c9ea40cc6a5b8e8d171c8
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-09-03 14:13:32 +0200

    new 404 page

---
+++ 404.php
@@ -0,0 +1,6 @@
+<?php get_header(); ?>
+    <div class="container cq-404 py-5 text-center">
+        <p><i class="fas fa-exclamation-triangle"></i> Ooops!</p>
+        <p>Erreur 404. On a rien pu trouvé. Désolé...</p>
+    </div>
+<?php get_footer();
