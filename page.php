a62d99cae9cbbf44b7102e9502cb502c9e77a477
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-09-06 13:46:26 +0200

    change default page template

--- page.php
+++ page.php
@@ -9,9 +9,9 @@ while (have_posts()) :
     the_post();
     ?>
 
-    <div class="container cq-contact my-5">
-        <h1 class="mb-3 cq-font text-center"><?php the_title() ?></h1>
-        <div class="mb-5 text-center"><?php the_content(); ?></div>
+    <div class="container cq-page py-5">
+        <h1 class="mb-5 text-center"><?php the_title() ?></h1>
+        <div class=""><?php the_content(); ?></div>
     </div>
 <?php
 endwhile;
