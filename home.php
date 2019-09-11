96c3bdf0efd1f0b9b97f1f83f189f33cafa483ea
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-09-11 21:06:37 +0200

    Minor change

--- home.php
+++ home.php
@@ -105,7 +105,7 @@
     <div id="homeContent" class="home-content py-5">
         <div class="home-latest container">
             <div class="text-center">
-                <h2 class="mb-4">À la une de la gazette</h2>
+                <h2 class="mb-4">La gazette de la Chouquette</h2>
             </div>
             <?php
             $latest_posts = new WP_Query('posts_per_page=6');
