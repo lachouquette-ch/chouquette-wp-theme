04d16214290ec0eccfbeb52a0b8fc30781720c93
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-10-11 17:19:00 +0200

    #92 add articles to fiche page

--- single-fiche.php
+++ single-fiche.php
@@ -10,14 +10,35 @@ while (have_posts()):
     $fiche = get_post();
     $fiche_category = chouquette_categories($fiche->ID)[0];
     $fiche_category_link = get_category_link($fiche_category->term_id);
+
+    $fichePosts = new WP_Query(array(
+        'meta_query' => array(
+            array(
+                'key' => CQ_FICHE_SELECTOR, // name of custom field
+                'value' => '"' . $fiche->ID . '"', // matches exactly "123", not just 123. This prevents a match for "1234"
+                'compare' => 'LIKE'
+            )
+        )
+    ));
     ?>
     <div id="app" class="container-fluid cq-single-fiche mx-auto py-4">
-        <?php
-        get_template_part('template-parts/fiche');
-        echo '<div class="mt-3 w-100 text-center">';
-        echo "<a href='$fiche_category_link' class='link-secondary' title='$fiche_category->name'>Découvre les autres fiches '$fiche_category->name'</a>";
-        echo '</div>';
-        ?>
+        <h1 class="text-center mb-4 cq-font"><?php the_title(); ?></h1>
+        <?php get_template_part('template-parts/fiche'); ?>
+        <div class="my-4 text-center">
+            <a href="<?php echo $fiche_category_link; ?>" role="button" class="btn btn-sm btn-outline-secondary px-4 py-2" title="<?php echo $fiche_category->name; ?>">
+                Découvre les autres fiches de la catégorie '<?php echo $fiche_category->name; ?>'</a>
+        </div>
+        <?php if ($fichePosts->have_posts()): ?>
+            <h2 class="text-center my-4 cq-font">Les articles associés</h2>
+            <div class="article-card-shuffle-container d-flex flex-wrap align-items-center justify-content-center mb-3">
+                <?php
+                while ($fichePosts->have_posts()) :
+                    $fichePosts->the_post();
+                    get_template_part('template-parts/article-card');
+                endwhile;
+                ?>
+            </div>
+        <?php endif; ?>
     </div>
 <?php
 endwhile;
