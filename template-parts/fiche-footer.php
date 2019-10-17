5febbc52a08b7d810a5059fcb3a1ff87bf4f9c21
Fabrice Douchant <fabrice.douchant@gmail.com>
2019-10-17 08:44:07 +0200

    #16 add article button to fiche services

--- template-parts/fiche-footer.php
+++ template-parts/fiche-footer.php
@@ -3,21 +3,20 @@
         // only for category pages or locations (not services)
         ?>
         <?php if (!empty($fiche_fields[CQ_FICHE_LOCATION])): ?>
-            <a href="#" class="btn btn-outline-secondary"
-               title="Voir la fiche sur la carte"
-               v-on:click.prevent="locateFiche(<?php echo $fiche->ID; ?>)">
-                <i class=" fas fa-map-marker-alt"></i>
-            </a>
-        <?php endif;
-        if (!empty($fichePosts)):
-            $lastest_post = $fichePosts[0];
-            ?>
-            <a href="<?php echo get_the_permalink($lastest_post); ?>"
-               title="Dernier article sur le lieu"
-               class="btn btn-outline-secondary">
-                <i class="far fa-newspaper"></i>
-            </a>
+        <a href="#" class="btn btn-outline-secondary" title="Voir la fiche sur la carte"
+           v-on:click.prevent="locateFiche(<?php echo $fiche->ID; ?>)">
+            <i class=" fas fa-map-marker-alt"></i>
+        </a>
         <?php endif; ?>
+    <?php endif; ?>
+    <?php if (!empty($fichePosts)):
+        $lastest_post = $fichePosts[0];
+        ?>
+        <a href="<?php echo get_the_permalink($lastest_post); ?>"
+           title="Dernier article sur le lieu"
+           class="btn btn-outline-secondary">
+            <i class="far fa-newspaper"></i>
+        </a>
     <?php endif; ?>
     <a href="#"
        title="Plus de détails (retourner la fiche)"
