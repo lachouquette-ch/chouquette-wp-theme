8bf157a8b318b9d6b6d4e5c4b517fe316dd92c85
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-05-19 17:29:48 +0200

    create simple redirection page

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="fr">
+<head>
+	<!-- Required meta tags -->
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+		  integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+	<link rel="stylesheet"
+		  href="https://fonts.googleapis.com/css?family=Lobster+Two|Shadows+Into+Light+Two|Lato&display=swap">
+
+	<style type="text/css" media="all">
+		.chouquette {
+			background-image: url('<?php echo get_template_directory_uri() . '/background.png' ?>');
+			background-size: cover;
+
+			font-family: Lato, sans-serif;
+			font-size: 1.2rem;
+			color: rgb(248, 239, 40);
+		}
+
+		h1 {
+			font-family: 'Lobster Two', sans-serif;
+		}
+	</style>
+
+	<title>Wordpress de La Chouquette</title>
+</head>
+<body>
+<div class="chouquette d-flex justify-content-center align-items-center vh-100 bg-secondary">
+	<div class="text-center p-3">
+		<h1 class="display-2 mb-4">La Chouquette</h1>
+		<p>Cher lecteur, il me semblerait que tu fasses erreur. Ceci est l'entrée des artistes... <i class="far fa-grin-tongue-wink"></i></p>
+		<p>Pour rejoindre notre très cher site</p>
+		<a class="btn btn-lg btn-light d-inline-block my-3" href="https://lachouquette.ch">Cliques ici !</a>
+		<p>A tout bientôt,</p>
+		<p class="float-right mt-3">- La Chouquette Team</p>
+	</div>
+</div>
+<div class="wordpress-admin m-2 fixed-bottom" style="right: 0; left: auto;">
+	<a class="text-white-50" href="/wp-admin">Lien administration</a>
+</div>
+
+<script crossorigin="anonymous"
+		integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+		src="https://code.jquery.com/jquery-3.4.1.slim.min.js"></script>
+<script crossorigin="anonymous"
+		integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+		src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+<script crossorigin="anonymous"
+		integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+		src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js"></script>
+</html>
