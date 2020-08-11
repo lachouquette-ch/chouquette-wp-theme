66365ec63746191cf5aa5e359f5e6551083cbeb3
Fabrice Douchant <fabrice.douchant@gmail.com>
2020-08-11 20:30:05 +0200

    add post-thumbnails and custom_logo features

--- functions.php
+++ functions.php
@@ -17,10 +17,29 @@ if (!function_exists('lachouquette_setup')) :
      */
     function lachouquette_setup()
     {
+        /*
+         * Enable support for Post Thumbnails on posts and pages.
+         *
+         * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+         */
+        add_theme_support('post-thumbnails');
+
         // This theme uses wp_nav_menu() in one location.
         register_nav_menus(array(
 			'primary-menu' => esc_html__('Menu principal', 'lachouquette'),
 			'footer-menu' => esc_html__('Menu footer', 'lachouquette'),
+        ));
+
+        /**
+         * Add support for core custom logo.
+         *
+         * @link https://codex.wordpress.org/Theme_Logo
+         */
+        add_theme_support('custom-logo', array(
+            'height' => 250,
+            'width' => 250,
+            'flex-width' => true,
+            'flex-height' => true,
         ));
     }
 endif;
